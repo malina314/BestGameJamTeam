a0a1a09a895c3288e6712674d2b23989512dc033
Krzysztof <hisboak@gmail.com>
2022-07-09 19:45:23 +0200

    Worrior now rotates to nearest enemy

--- Assets/Scripts/Entities/WarriorEntity.cs
+++ Assets/Scripts/Entities/WarriorEntity.cs
@@ -9,9 +9,48 @@ public class WarriorEntity : Entity
 
     void Update()
     {
+        Debug.Log("B PRESSED");
         if (Input.GetKeyDown(KeyCode.D))
         {
-            TryToAttack<EnemyEntity>();
+            this.TryToAttack<EnemyEntity>();
+        }
+    }
+
+    new public void TryToAttack<T>() where T : Entity
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Range);
+        double distance = 2*Range;
+        Entity? target = null;
+        foreach (var collider in colliders)
+        {
+            if (collider.TryGetComponent<T>(out var entity))
+            {
+                if (!entity.gameObject.Equals(gameObject))
+                {
+                    float xx = entity.gameObject.transform.position.x - gameObject.transform.position.x;
+                    float yy = entity.gameObject.transform.position.y - gameObject.transform.position.y;
+                    float zz = entity.gameObject.transform.position.z - gameObject.transform.position.z;
+                    float dist = Mathf.Sqrt(xx * xx + yy * yy + zz * zz);
+
+                    if(distance > dist)
+                    {
+                        distance = dist;
+                        target = entity;
+                    }
+                }
+            }
+        }
+        if(target != null)
+        {
+            Debug.Log($"Target {target.name}");
+            if( target.transform.position.x > gameObject.transform.position.x)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            } 
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
     }
 }
